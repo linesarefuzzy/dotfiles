b574570bde5f54b0ce371dd0791e120b78dfa2ee
Adam Konner <fuzzy@ip-172-31-52-208.ec2.internal>
2016-02-17 06:38:37 +0000

    bugs etc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=dotfiles                    # dotfiles directory
+olddir=dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
 files=".profile .screenrc .vimrc .vim"
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+prevdir=`pwd`
+cd ~
 mkdir -p $olddir
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+# move any existing dotfiles in homedir to dotfiles_old directory 
 for file in $files; do
-  echo "$file"
-  mv ~/.$file ~/dotfiles_old/
-  ln -s $dir/$file ~/.$file
+  echo "Moving $file to $olddir"
+  mv -i ~/$file ~/dotfiles_old/
+done
+
+# create symlinks
+for file in $files; do
+  echo "Creating symlink for $file in ~"
+  ln -si $dir/$file ~/
 done
 
 #mkdir -p ~/bin
@@ -27,5 +28,3 @@ done
 #curl http://get.zedapp.org | bash
 
 cd $prevdir
-
-source ~/.profile
