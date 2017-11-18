3d41d8ab1d9b3e3816fa3eb7aa8866e6b063e19b
Adam Konner <linesarefuzzy@gmail.com>
2017-11-18 16:15:20 -0500

    install zsh issue

--- install.sh
+++ install.sh
@@ -5,14 +5,19 @@ dir=dotfiles                    # dotfiles directory
 olddir=dotfiles-pre-fuzzy       # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files="profile bash_profile zshrc screenrc vimrc vim gitconfig gitignore"
+files="profile zshrc screenrc vimrc vim gitconfig gitignore"
 
 prevdir=`pwd`
 cd ~
 mkdir -p $olddir
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Use this for now in order to not start zsh at the end (https://github.com/robbyrussell/oh-my-zsh/issues/5873)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch || {
+  echo "Could not install Oh My Zsh" >/dev/stderr
+  exit 1
+}
 
 # move any existing dotfiles in homedir to dotfiles_old directory
 for file in $files; do
