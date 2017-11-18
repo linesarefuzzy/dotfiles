2890f7210dfcb347454375fbdaede8463959f674
Adam Konner <linesarefuzzy@gmail.com>
2017-11-18 16:01:50 -0500

    Uninstall script bugs

--- uninstall.sh
+++ uninstall.sh
@@ -9,21 +9,25 @@ files="profile bash_profile zshrc screenrc vimrc vim gitconfig gitignore"
 prevdir=`pwd`
 cd ~
 
-# move any old dotfiles back and remove links
+# Remove links
 for file in $files; do
-  if [ -f .$file ] && [ -L .$file ]; then
+  if [ -f ~/.$file ] && [ -L ~/.$file ]; then
     echo "Removing symlink .$file"
-    rm -i .$file
+    rm ~/.$file
   fi
-  echo "Moving old dotfiles back to home"
-  mv -i ~/$olddir/.* ~/
 done
+
+echo "Moving old dotfiles back to home"
+shopt -s dotglob nullglob
+mv -i ~/$olddir/* ~/
+shopt -u dotglob nullglob
 
 # Remove dotfiles dir
 rm -rf $dir
 
 # Uninstall oh-my-zsh
 uninstall_oh_my_zsh
+rm -rfi ~/.oh-my-zsh
 
 echo "Change shell to bash?"
 select yn in "Yes" "No"; do
